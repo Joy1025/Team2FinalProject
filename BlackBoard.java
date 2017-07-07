cce60835f30d4f8c15201bb64ea4b57677082096
Joy1025 <jingyili@asu.edu>
2017-07-07 15:30:24 -0700

    Modified update based on ExamBarin

--- BlackBoard.java
+++ BlackBoard.java
@@ -2,30 +2,27 @@ package CSE360;
 
 import java.util.Observable;
 import java.util.Observer;
+import java.util.*;
 
 public class BlackBoard extends Observable implements Observer {
 	private boolean[] correctorNot= new boolean[10];
-	private double[] time = new double[10];
+	private long[] time = new long[10];
 	private int count;
 	private int numcorrect;
-	
+	/*getter and setter for passing value to CompanionBrain*/
 	public boolean[] getCorrectorNot(){
 		return correctorNot;
 	}
 	public void setCorrectorNot(boolean[] correctorNot){
 		this.correctorNot=correctorNot;
-		count++;//increase the # of question answered. 
 	}
-	public double[] getTime(){
+	public long[] getTime(){
 		return time;
 	}
-	public void setTime(double[] time){
+	public void setTime(long[] time){
 		this.time=time;
 	}
-	public void updateall(boolean[] correct, double[] newtime) {
-		setTime(newtime);
-		setCorrectorNot(correct);	
-		}
+
 	//get how many question answered
 	public int getQuesAnswered() {
 		return count;
@@ -41,8 +38,18 @@ public class BlackBoard extends Observable implements Observer {
 		}
 	}
 	@Override
+	/*Update time,correctness from ExamBrain*/
 	public void update(Observable o, Object arg) {
-		java.lang.System.out.println("update called");
-		//need change based on examPanel
+		time = ((ExamBrain)o).getlastElaspsedTime();
+		int[] selectionList=((ExamBrain)o).getSelectionList();
+		int[] answerList=((ExamBrain)o).getCorrectAnswer();
+		for(int i = 0; i<correctorNot.length;i++){
+			if(selectionList[i]==answerList[i]){
+				correctorNot[i]=true;
+			}
+			count++;//increase the # of question answered. 
+		}
+		
 	}
 }
+
